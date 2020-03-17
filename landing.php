200103843866aafa728ca0deb48f53c6e588a506
Abigail <abbyliguma@gmail.com>
2020-03-17 09:26:23 +0200

    changed title name

--- landing.php
+++ landing.php
@@ -1,9 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing-page</title>
+  <head>
+    <title>Hotel Monkey</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+
+    <!--Font links-->
+    <link href="https://fonts.googleapis.com/css?family=Cabin+Condensed|Gentium+Book+Basic|Playfair+Display+SC|Prata|Sanchez|Vidaloka&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
     
