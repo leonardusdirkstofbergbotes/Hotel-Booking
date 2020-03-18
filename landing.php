d6ab2bf08af265fb773560271f960259985b6bef
Abigail <abbyliguma@gmail.com>
2020-03-18 17:00:16 +0200

    form update & hero video added

--- landing.php
+++ landing.php
@@ -1,55 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Hotel Monkey</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="css/styles.css" type="text/css">
-
-    <!--Font links-->
-    <link href="https://fonts.googleapis.com/css?family=Cabin+Condensed|Gentium+Book+Basic|Playfair+Display+SC|Prata|Sanchez|Vidaloka&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
-
-  <body>
-      <div class="landing-container">
-
-        <!--Form-->
+<body>
+    
+    <video autoplay muted loop class="myVideo">
+        <source src="video1.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+      </video>
         
-        <div class="cover-container">
+    <div class="container">
 
-          <h1 id="title">Hotel Monkey</h1>
-
-            <form action="/action_page.php" class="form">
-             <label for="fname">First Name</label>
-              <input type="text" id="fname" name="firstname" placeholder="Your name..">
-        
-             <label for="lname">Last Name</label>
-             <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-            
-            <label for="fname">Email Address</label>
-            <input type="text" id="fname" name="email" placeholder="Your Email..">
-
-            
-            <label for="In">Book In date</label>
-            <input type="date" id="date" name="date">
-
-            <label for="Out">Book out date</label>
-            <input type="date" id="date" name="date"><br/>
-        
-            <input type="submit" value="Submit">
-          </form>
-        </div>
+        <form action="#">
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name..">
       
-        
-         </div>
-       </head>
+           <label for="lname">Last Name</label>
+           <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+          
+          <label for="fname">Email Address</label>
+          <input type="text" id="fname" name="email" placeholder="Your Email..">
+
+          <label for="In">Book In date</label>
+          <input type="date" id="date" name="date">
+
+          <label for="Out">Book out date</label>
+          <input type="date" id="date" name="date">
     
-      </div>
+          <button>Submit</button>
+        </form>
+
+    </div>
     
-  </body>
+</body>
 </html>
