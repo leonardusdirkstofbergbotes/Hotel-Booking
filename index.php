45d71fbd06989eb751f1670f5f8f1791f1f600cd
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-03-21 10:33:54 +0200

    set all inputs to require and set the minimum input date to TODAY

--- index.php
+++ index.php
@@ -29,24 +29,28 @@
 
         <form method="post">
             <label for="fname">First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." required>
       
            <label for="lname">Last Name</label>
-           <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+           <input type="text" id="lname" name="lastname" placeholder="Your last name.." required>
           
           <label for="fname">Email Address</label>
-          <input type="text" id="fname" name="email" placeholder="Your Email..">
+          <input type="text" id="fname" name="email" placeholder="Your Email.." required>
 
           <label for="In">Book In date</label>
-          <input type="date" id="date" name="in">
+          <input type="date" id="datein" name="in" required>
 
           <label for="Out">Book out date</label>
-          <input type="date" id="date" name="out">
+          <input type="date" id="dateout" name="out" required>
     
           <button type="submit" name="lets_start">Submit</button>
         </form>
 
     </div>
     
+    <script>
+    datein.min = new Date().toISOString().split("T")[0];
+    dateout.min = new Date().toISOString().split("T")[0];
+    </script>
 </body>
 </html>
