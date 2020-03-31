fa4707d6fbb787d0ba5411ae916f54bb6ac326de
Abigail <abbyliguma@gmail.com>
2020-03-31 18:03:24 +0200

    monkey image added to title

--- calc.php
+++ calc.php
@@ -7,7 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compare Hotels</title>
+    <title>Hotel Monkey</title>
+    <link rel="icon" type="image/gif/png" href="./images/monkey.png">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
